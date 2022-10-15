5830b9dd21ce63744c76269646cd0ab217a94fd5
Nsongurua <wazobiadoctor@gmail.com>
2022-10-15 10:56:41 -0700

    c scripting

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - script c for print.
+ * main - script c print.
  *
  * return : Always 0 (Succesful)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+	'\n'
 	return (0);
 }
