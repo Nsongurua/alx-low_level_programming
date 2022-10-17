5bc8fa28c397449121c2f26ce436d62aa8a89ea3
Nsongurua <wazobiadoctor@gmail.com>
2022-10-17 13:40:52 -0700

    c scripting

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - scripting in c
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int z;
+
+	z = 'z';
+
+	while (z => 'a')
+	{
+		putchar(z);
+		++z;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
