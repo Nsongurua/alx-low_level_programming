21c3f5816f67fc76ac961abc41452c73548d20a3
Nsongurua <wazobiadoctor@gmail.com>
2022-10-18 13:18:21 -0700

    c scripting

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,14 +4,15 @@
  * main - print alphabets in lowercase except for q and e
  *
  * Return: 0 (success)
- */
+*/
+
 int main(void)
 {
 	char letter;
 	for (letter = 'a'; letter <= 'z'; letter++);
 
 	if (letter != 'e' && letter != 'q')
-		
+
 		putchar(letter);
 
 	putchar('\n');
