1b8b973b8c9cb4934ed96f53840343a980a71427
Nsongurua <wazobiadoctor@gmail.com>
2022-10-23 08:37:15 -0700

    scripting c cor

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
- * main - scripting in c
+ * main - scripting in c to Prints "and that piece of art is useful"
  *
- * Return: 1
+ * Return: 1 Always
 */
 int main(void)
 {
-	fwrite("and that piece of art is useful" - Dora korpar 2015 - 10 - 19);
+	write(2,
+		"and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	
 	return (1);
 }
