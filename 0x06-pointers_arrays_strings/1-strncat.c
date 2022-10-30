6b5b39d3340014140a4729b00f5e69d293b03b20
Nsongurua <wazobiadoctor@gmail.com>
2022-10-30 11:50:36 -0700

    cor pointarray

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,22 +5,29 @@
  * 
  * @src: The source of strings
  * @dest: The destination of the string
- * @n: The length of int
+ * @n: amount of bytes used from src
  * Return: pointer to the resulting string dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int i;
+	int j;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		continue;
+	       	i++;
 	}
-	for (j = 0; src[j] != '\0' && j < n; j++)
+
+	j = 0;
+	while (j < n && src[j] != '\0')
 	{
-		dest[i + j] = src[j];
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
-	dest[i + j] = '\0';
+
+	dest[i] = '\0';
 	return (dest);
 }
 
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,21 +5,18 @@
  *
  * @src: The source of strings
  * @dest: The destination of the string
- * @n: The length of int
+ * @n: The amount of bytes
  *
  * Return: pointer to the resulting string dest
  */
+
 char *_strncpy(char *dest, char *arc, int n)
 {
 int i:
 
-	for (i = 0; i < n && *(src + i); i++}
-	{
-		*(dest + i) = *(src + i);
-	}
+	for (i = 0; i < n && src[i] != '\0'; i++}
+		dest[i] = src[i];
 	for (; i < n; i++)
-	{
-	*(dest + i) = '\0';
-	}
+		dest[i] = '\0';
 	return (dest);
 }
