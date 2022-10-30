49341084a0a762940ced00d19727a662775f53ea
Nsongurua <wazobiadoctor@gmail.com>
2022-10-30 12:04:00 -0700

    cor array

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,18 +13,18 @@ char *_strncat(char *dest, char *src, int n)
 	int j;
 
 	i = 0;
-	while (dest[i] != '\0')
-	{
-	      i++;
-	}
+		while (dest[i] != '\0')
+		{
+	      		i++;
+		}
 
 	j = 0;
-	while (j < n && src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
+		while (j < n && src[j] != '\0')
+		{
+			dest[i] = src[j];
+			i++;
+			j++;
+		}
 
 	dest[i] = '\0';
 	return (dest);
