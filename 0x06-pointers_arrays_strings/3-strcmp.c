15fd4674db7566b31112734c445676e5d2479960
Nsongurua <wazobiadoctor@gmail.com>
2022-10-30 12:32:22 -0700

    cor array

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,25 +1,20 @@
 #include "main.h"
 /**
  * _strcmp - function to compare two strings
- * @s1:string
- * @s2:strmp
- * Return:int
+ * @s1: firt string
+ * @s2: second string
+ * Return: 0 if s1 and s2 are equals, another number if not
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
-	{
-		s1++;
-		s2++;
-	}
+	int i;
 
-	if(*s1 == *s2)
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
-		return (0);
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}		
 	}
-
-	else
-	{
-		return (*s1 - *s2);
-	}
+	return (0);
 }
