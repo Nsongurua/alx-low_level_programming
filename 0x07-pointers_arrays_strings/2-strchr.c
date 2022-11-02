131b515b6b627b91c74f97c624135186793a903c
Nsongurua <wazobiadoctor@gmail.com>
2022-11-01 18:51:47 -0700

    task 7

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,17 +13,17 @@
 char *_strchr(char *s, char c)
 
 {
-	if(s == NULL)
+	if (s == NULL)
 		return (NULL);
 	for (; *s; s++)
 
 	{
-		if(*s == c)
+		if (*s == c)
 			return (s);
 	}
-	if (*s == c)
+		if (*s == c)
 	{
-		 return (s);
+		return (s);
 	}
 	return (NULL);
 }
